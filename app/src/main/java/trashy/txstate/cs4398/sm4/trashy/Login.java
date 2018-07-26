91f78c153fb7441f90aca02a1075f3b119683d0f
PenguinTamer <PenguinTamer@users.noreply.github.com>
2018-07-25 22:22:23 -0500

    Added demo navigation from login activity to Trashy activity (camera activity)

--- app/src/main/java/trashy/txstate/cs4398/sm4/trashy/Login.java
+++ app/src/main/java/trashy/txstate/cs4398/sm4/trashy/Login.java
@@ -1,13 +1,38 @@
 package trashy.txstate.cs4398.sm4.trashy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
+    //Handles
+    /**Handle for username field*/
+    private EditText userNameField;
+    private EditText passwordField;
+    private Button signInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //Linking handles
+        userNameField = findViewById(R.id.userNameField);
+        signInButton = findViewById(R.id.signInButton);
+
+        //Assigning Listeners
+        //Sign in button listener
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent changeActivity;
+
+                changeActivity = new Intent(Login.this, Trash.class);
+                startActivity(changeActivity);
+            }
+        });
     }
 }
