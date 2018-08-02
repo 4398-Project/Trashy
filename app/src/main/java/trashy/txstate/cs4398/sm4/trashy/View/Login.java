788cce12d7209331a91267a914fed2709ad0b573
PenguinTamer <PenguinTamer@users.noreply.github.com>
2018-08-02 13:23:36 -0500

    Merged w/ Mj's code (MJ3 Branch) -- linked up to firebase for Auth (Working)

--- app/src/main/java/trashy/txstate/cs4398/sm4/trashy/View/Login.java
+++ app/src/main/java/trashy/txstate/cs4398/sm4/trashy/View/Login.java
@@ -6,41 +6,182 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.FirebaseAuth;
+import android.widget.Toast;
+import android.util.Log;
+import com.google.firebase.auth.AuthResult;
+import android.support.annotation.NonNull;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.OnCompleteListener;
+import java.lang.*;
 import trashy.txstate.cs4398.sm4.trashy.R;
 
 public class Login extends AppCompatActivity {
     //Handles
     /**Handle for username field*/
     private EditText userNameField;
-    /**Handle for password field*/
     private EditText passwordField;
-    /**Handle for sign in button*/
     private Button signInButton;
-    /**Handle for register button*/
     private Button registerButton;
+    private FirebaseAuth mAuth;
+    private String TAG;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
 
         //Linking handles
         userNameField = findViewById(R.id.userNameField);
+        passwordField = findViewById(R.id.passwordField);
         signInButton = findViewById(R.id.signInButton);
+        registerButton = findViewById(R.id.registerButton);
 
         //Assigning Listeners
         //Sign in button listener
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                /**Intent used to specify which activity to jump to next when signInButton is pressed*/
-                Intent changeActivity;
 
-                changeActivity = new Intent(Login.this, Trash.class);
-                startActivity(changeActivity);
+                if (userNameField.getText().toString().isEmpty() || passwordField.getText().toString().isEmpty()) {
+                    // Sign in fails due to one of the credentials being empty
+                    Log.d(TAG, "Sign in failed. A sign in field is empty.");
+                    Toast.makeText(Login.this, "Registration failed.  E-mail/Password cannot be blank.",
+                            Toast.LENGTH_LONG).show();
+                    updateUI(null);
+                } else {
+                    mAuth.signInWithEmailAndPassword(userNameField.getText().toString(), passwordField.getText().toString())
+                            .addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (rootUser(userNameField.getText().toString(), passwordField.getText().toString())) {
+                                        Log.d(TAG, "rootUserDetected");
+                                        Toast.makeText(Login.this, "Root User Detected.",
+                                                Toast.LENGTH_LONG).show();
+                                    } else if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        Log.d(TAG, "signInWithEmail:success");
+                                        Toast.makeText(Login.this, "Sign in successful.",
+                                                Toast.LENGTH_LONG).show();
+                                        FirebaseUser user = mAuth.getCurrentUser();
+                                        updateUI(user);
+
+                                        Intent changeActivity;
+
+                                        changeActivity = new Intent(Login.this, Trash.class);
+                                        startActivity(changeActivity);
+                                    } else {
+                                        // Sign in fails due to incorrect credentials or error connecting to server
+                                        Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                        if (task.getException().toString().contains("The password is invalid or the user does not have a password."))
+                                            Toast.makeText(Login.this, "Incorrect password.",
+                                                    Toast.LENGTH_LONG).show();
+                                        else if (task.getException().toString().contains("The email address is badly formatted."))
+                                            Toast.makeText(Login.this, "Invalid e-mail address. Check formatting.",
+                                                    Toast.LENGTH_LONG).show();
+                                        else if (task.getException().toString().contains("There is no user record corresponding to this identifier."))
+                                            Toast.makeText(Login.this, "E-mail not found. Click \"Register\" to register a new e-mail.",
+                                                    Toast.LENGTH_LONG).show();
+                                        else
+                                            Toast.makeText(Login.this, "Sign in failed for undetermined reason. Check internet connection.",
+                                                    Toast.LENGTH_LONG).show();
+                                        updateUI(null);
+                                    }
+
+                                    // ...
+                                }
+                            });
+                }
             }
         });
 
+        //Register button listener
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                //Retrieving user entered values
+
+                if (userNameField.getText().toString().isEmpty() || passwordField.getText().toString().isEmpty()) {
+                    // Sign in fails due to one of the credentials being empty
+                    Log.d(TAG, "Sign in failed. A sign in field is empty.");
+                    Toast.makeText(Login.this, "Registration failed.  E-mail/Password cannot be blank.",
+                            Toast.LENGTH_LONG).show();
+                    updateUI(null);
+                } else {
+                    mAuth.createUserWithEmailAndPassword(userNameField.getText().toString(), passwordField.getText().toString())
+                            .addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        Log.d(TAG, "createUserWithEmail:success");
+                                        Toast.makeText(Login.this, "Registration successful.",
+                                                Toast.LENGTH_LONG).show();
+                                        FirebaseUser user = mAuth.getCurrentUser();
+                                        updateUI(user);
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Log.w(TAG, "createUserWithEmail:failure\n\n" + task.getException().toString() +  "\n\n", task.getException());
+                                        if (task.getException().toString().contains("The email address is already in use by another account."))
+                                            Toast.makeText(Login.this, "This email address is already in use by another account.",
+                                                    Toast.LENGTH_LONG).show();
+                                        else if (task.getException().toString().contains("The given password is invalid."))
+                                            Toast.makeText(Login.this, "The given password is invalid. It must contain at least 6 characters.",
+                                                    Toast.LENGTH_LONG).show();
+                                            //Note: Not sure if Login correct here
+                                        else
+                                            Toast.makeText(Login.this, "Registration failed for undetermined reason. Check internet connection.",
+                                                    Toast.LENGTH_LONG).show();
+                                        updateUI(null);
+                                    }
+
+                                    // ...
+                                }
+                            });
+                }
+            }
+        });
+
+    }
+
+    private boolean rootUser(String userName, String password){
+        if (userNameField.getText().toString().equals("mmr161@txstate.edu") && passwordField.getText().toString().equals("rootaccess")) {
+            return true;
+        } else if (userNameField.getText().toString().equals("acs144@txstate.edu") && passwordField.getText().toString().equals("rootaccess")) {
+            return true;
+        } else if (userNameField.getText().toString().equals("dlr195@txstate.edu") && passwordField.getText().toString().equals("rootaccess")) {
+            return true;
+        } else if (userNameField.getText().toString().equals("m_v188@txstate.edu") && passwordField.getText().toString().equals("rootaccess")) {
+            return true;
+        }
+        return false;
+    }
+
+    //TODO
+    private void updateUI(FirebaseUser user) {
+        /*hideProgressDialog();
+        if (user != null) {
+            mStatusTextView.setText(getString(R.string.google_status_fmt, user.getEmail()));
+            mDetailTextView.setText(getString(R.string.firebase_status_fmt, user.getUid()));
+            findViewById(R.id.sign_in_button).setVisibility(View.GONE);
+            findViewById(R.id.sign_out_and_disconnect).setVisibility(View.VISIBLE);
+        } else {
+            mStatusTextView.setText(R.string.signed_out);
+            mDetailTextView.setText(null);
+            findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
+            findViewById(R.id.sign_out_and_disconnect).setVisibility(View.GONE);
+        }*/
     }
 }
