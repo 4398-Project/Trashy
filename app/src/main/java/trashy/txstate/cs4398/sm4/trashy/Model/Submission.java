00851451ceb47524f9c63a6bac3cbaaddce617c6
alek <alek.salazar@icloud.com>
2018-08-02 11:18:54 -0500

    updated added documentation skeletons to sub and user classes

--- app/src/main/java/trashy/txstate/cs4398/sm4/trashy/Model/Submission.java
+++ app/src/main/java/trashy/txstate/cs4398/sm4/trashy/Model/Submission.java
@@ -2,6 +2,9 @@ package trashy.txstate.cs4398.sm4.trashy.Model;
 
 import java.util.ArrayList;
 
+/**
+ * The type Submission.
+ */
 public class Submission {
     private Integer id;
     private Integer lastId = 0; // must be pulled from DB
@@ -13,29 +16,60 @@ public class Submission {
         //gets last id from server
         return 0;
     }
+
+    /**
+     * Instantiates a new Submission.
+     *
+     * @param user the user
+     */
     public Submission(User user) {
         this.user = user;
         //lastID must be pulled from server
         id = getLastId() + 1;
     }
 
+    /**
+     * Add trash item.
+     *
+     * @param trashItem the trash item
+     */
     public void addTrashItem(TrashItem trashItem){
         this.trashBin.add(trashItem);
         this.totalPoints += trashItem.getPointWorth();
     }
 
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
     public Integer getId() {
         return id;
     }
 
+    /**
+     * Gets total points.
+     *
+     * @return the total points
+     */
     public Integer getTotalPoints() {
         return totalPoints;
     }
 
+    /**
+     * Gets user.
+     *
+     * @return the user
+     */
     public User getUser() {
         return user;
     }
 
+    /**
+     * Gets trash bin.
+     *
+     * @return the trash bin
+     */
     public ArrayList<TrashItem> getTrashBin() {
         return trashBin;
     }
--- app/src/main/java/trashy/txstate/cs4398/sm4/trashy/Model/User.java
+++ app/src/main/java/trashy/txstate/cs4398/sm4/trashy/Model/User.java
@@ -1,25 +1,53 @@
 package trashy.txstate.cs4398.sm4.trashy.Model;
 
+/**
+ * The type User.
+ */
 public class User {
     private String username;
     private Integer totalPoints;
 
+    /**
+     * Gets username.
+     *
+     * @return the username
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Sets username.
+     *
+     * @param username the username
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Gets total points.
+     *
+     * @return the total points
+     */
     public Integer getTotalPoints() {
         return totalPoints;
     }
 
+    /**
+     * Add points.
+     *
+     * @param pts the pts
+     */
     public void addPoints(Integer pts){
         this.totalPoints += pts;
     }
 
+    /**
+     * Sub points.
+     *
+     * @param pts the pts
+     */
     public void subPoints(Integer pts){
         if ((this.totalPoints - pts) >= 0){
             this.totalPoints -= pts;
