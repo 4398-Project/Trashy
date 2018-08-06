9034b758460d08b9c16f00baccbb5b7ea5beced4
PenguinTamer <PenguinTamer@users.noreply.github.com>
2018-08-06 12:25:52 -0500

    initial addition of db interface for upload of submissions

---
+++ app/src/main/java/trashy/txstate/cs4398/sm4/trashy/Controller/DBInterface.java
@@ -0,0 +1,20 @@
+package trashy.txstate.cs4398.sm4.trashy.Controller;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import trashy.txstate.cs4398.sm4.trashy.Model.Submission;
+
+public class DBInterface {
+    FirebaseDatabase database;
+    DatabaseReference reference;
+
+    public DBInterface(FirebaseDatabase database, DatabaseReference reference) {
+        this.database = database;
+        this.reference = reference;
+    }
+
+    public void uploadSubmission(Submission submission){
+
+    }
+}
