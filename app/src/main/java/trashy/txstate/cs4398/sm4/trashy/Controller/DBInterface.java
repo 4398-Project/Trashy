91b19e36c56c2713f75f91b7f30f72405b678b82
alek <alek.salazar@icloud.com>
2018-08-07 02:13:31 -0500

    De-bugging read last sub from DB

--- app/src/main/java/trashy/txstate/cs4398/sm4/trashy/Controller/DBInterface.java
+++ app/src/main/java/trashy/txstate/cs4398/sm4/trashy/Controller/DBInterface.java
@@ -1,7 +1,13 @@
 package trashy.txstate.cs4398.sm4.trashy.Controller;
 
+import android.support.annotation.NonNull;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import trashy.txstate.cs4398.sm4.trashy.Model.Submission;
 
@@ -15,7 +21,20 @@ public class DBInterface {
         this.reference = database.getReference("Subs");
 
         //Get last id;
-        lastID = 0;
+        lastID = 11;
+
+        Query lastQ = reference.child("submissions").orderByValue().limitToLast(1);
+        lastQ.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                lastID = Integer.parseInt(dataSnapshot.child("id").getValue(String.class));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                lastID = 10;
+            }
+        });
     }
 
     public void uploadSubmission(Submission submission) {
